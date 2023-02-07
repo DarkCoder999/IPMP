71fa65e58d398ae52391c9a87653e3a1ec2b57e8
DarkCoder999 <98643503+DarkCoder999@users.noreply.github.com>
2023-02-07 18:57:49 +0530

    Create BinarySearch_1.cpp

---
+++ Week1/BinarySearch_1.cpp
@@ -0,0 +1,19 @@
+#https://leetcode.com/problems/binary-search/description/
+#Problem 704
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, h = nums.size() - 1;
+        while(l <= h){
+            int mid = l + (h - l)/2;
+            if(nums[mid] == target)
+                return mid;
+            if(nums[mid] > target)
+                h = mid - 1;
+            else
+                l = mid + 1;
+        }
+        return -1;
+    }
+};
