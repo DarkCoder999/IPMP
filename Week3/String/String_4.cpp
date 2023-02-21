d7388dbf90f98ebbfc4c8892b2cf62d6e0fb819c
DarkCoder999 <98643503+DarkCoder999@users.noreply.github.com>
2023-02-21 23:09:47 +0530

    Reverse words

---
+++ Week3/String/String_4.cpp
@@ -0,0 +1,14 @@
+/**
+https://leetcode.com/problems/reverse-words-in-a-string/description/
+**/
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string word, ans;
+        while(ss >> word){
+            ans = word + " " + ans;
+        }
+        return ans.substr(0, ans.size()-1);
+    }
+};
