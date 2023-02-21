0f63b3fcb6eb9556ccb40a11d217ceb6143f430d
DarkCoder999 <98643503+DarkCoder999@users.noreply.github.com>
2023-02-21 19:51:03 +0530

    Create String_3.cpp

---
+++ Week3/String/String_3.cpp
@@ -0,0 +1,13 @@
+/**
+https://leetcode.com/problems/first-unique-character-in-a-string/
+**/
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        for(int i=0;i<s.length();i++){
+            if(s.find(s[i], s.find(s[i])+1) == string::npos)
+                return i;
+        }
+        return -1;
+    }
+};
