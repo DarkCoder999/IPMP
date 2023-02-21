61194f51c31dfa303a180d50f27be3f62a3798d2
DarkCoder999 <98643503+DarkCoder999@users.noreply.github.com>
2023-02-21 23:28:58 +0530

    Print Duplicates

---
+++ Week3/String/String_6.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string getDuplicateLetters(string s) {
+        unordered_map <char, int> c;
+        for(int i=0;i<s.length();i++)
+        c[s[i]]++;
+        string x = "";
+        for(auto& it : c){
+            if(it.second > 1)
+            x+=it.first;
+        }
+        return x;
+    }
+};
