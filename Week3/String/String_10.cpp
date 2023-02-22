ca9a2dd32646c8f8f04168f74a42da1bf6f67cbc
DarkCoder999 <98643503+DarkCoder999@users.noreply.github.com>
2023-02-22 22:40:18 +0530

    String difference

--- Week3/String/String_10.cpp
+++ Week3/String/String_10.cpp
@@ -1,14 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    vector<int> v(26,0);
     string s1,s2;
     string s3="";
     cin>>s1>>s2;
-    unordered_map<char,int> m;
     for(int i=0;i<s2.length();i++)
-    m[s2[i]]=1;
+    v[s2[i]-'a']=1;
     for(int i=0;i<s1.length();i++){
-        if(m[s1[i]]!=1)
+        if(v[s1[i]-'a']!=1)
         s3+=s1[i];
     }
     cout<<s3;
